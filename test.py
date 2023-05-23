1d16481e05e5c548582e368409557231122264b8
Sofia Jain <sofiasjain@gmail.com>
2023-05-23 16:19:40 -0700

    simple pakcage

---
+++ test.py
@@ -0,0 +1,10 @@
+def add(x,y):
+    """add two numbers
+
+    Args:
+        x (int): an integer number
+        y (int): an integer number
+    Returns:
+        int: the sum of two integers
+    """
+    
